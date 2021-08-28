d25b37067a6ba4ef251670892eb2882ae4b3268d
cuongngm <cuonghip0908>
2021-08-28 15:04:34 +0700

    feat: learn build logger

---
+++ src/logger/logger.py
@@ -0,0 +1,17 @@
+import logging
+import logging.config
+from pathlib import Path
+from src.utils.utils_function import read_json
+
+
+def setup_logging(save_dir, log_config='logger_config.json', default_level=logging.INFO):
+    log_config = Path(__file__).parent.joinpath(log_config)
+    if log_config.is_file():
+        config = read_json(log_config)
+        for _, handler in config['handlers'].items():
+            if 'filename' in handler:
+                handler['filename'] = str(save_dir / handler['filename'])
+        logging.config.dictConfig(config)
+    else:
+        print('Warning: logging configuration file is not found in {}'.format(log_config))
+        logging.basicConfig(level=default_level)
